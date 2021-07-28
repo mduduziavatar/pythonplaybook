4ee7906b888055262dcc24213c7832c95d0c3284
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2021-07-28 20:56:33 +0200

    more knowledge

---
+++ arrays.py
@@ -0,0 +1,4 @@
+names = [5]
+names[0] = "Siphiwe"
+names[1] = "Malinga"
+print(names[0])
---
+++ score.py
@@ -0,0 +1,9 @@
+score = float(input('Score '))
+if score >= 1:
+    print("the score is out of range!")
+elif score >= 0.9:
+    print("A grade")
+elif score >= 0.8:
+    print("B grade")
+else:
+    print("All good!!")
